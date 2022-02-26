5017e9c1e0040abe0bed682c55db898032fae397
adarsh <adarshurmaliya@gmail.com>
2022-02-26 18:47:14 +0530

    cors Error Fixed

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 app.use(express.static('public')); //to tell location of css files
@@ -9,6 +10,13 @@ app.use(express.json()); //make sure it take json file formate from body of req
 const connectDb = require('./config/db');
 connectDb();
 
+// CORS
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+    // ['http://localhost:3000', 'http://localhost:5000']
+}
+
+app.use(cors(corsOptions));
 // Configure ejs- Template Engine
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
