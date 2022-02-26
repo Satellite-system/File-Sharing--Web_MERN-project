f4b9bb972f981d5d13ea591ec479dd65a08d2311
adarsh <adarshurmaliya@gmail.com>
2022-02-26 23:18:41 +0530

    url updated

--- public/js/index.js
+++ public/js/index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://innshare.herokuapp.com";
+const baseURL = "https://speedy-share.herokuapp.com/";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
